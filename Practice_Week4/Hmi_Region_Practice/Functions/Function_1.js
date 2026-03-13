074f7981f112a01398c0e4a3ebb240ee1253c814
kameshwarank-intimetec <kameshwaran.k@intimetec.com>
2026-03-13 17:12:45 +0530

    user control, action template and function binding examples

---
+++ Practice_Week4/Hmi_Region_Practice/Functions/Function_1.js
@@ -0,0 +1,23 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var Function_1 = function (par1) {
+        if (par1 > 0) {
+            return "Positive";
+        } else if (par1 < 0) {
+            return "Negative";
+        } else {
+            return "Zero";
+        }
+    };
+    
+    TcHmi.Functions.registerFunction('Function_1', Function_1);
+})(TcHmi);

872db968cf073add987685e7266a8e581aab8aa9
kameshwarank-intimetec <kameshwaran.k@intimetec.com>
2026-03-16 08:58:31 +0530

    implemented logic for assignment 4

---
+++ Assignment_Week4/Machine_HMI/Functions/EnumValueConverter.js
@@ -0,0 +1,26 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var EnumValueConverter = function (par1) {
+        let stateNames = [
+            "IDLE",              
+            "WAIT_FOR_BOTTLE",   
+            "FILLING",           
+            "COMPLETED",         
+            "STOPPED",           
+            "ERROR"              
+        ];
+
+        return stateNames[par1] || "UNKNOWN STATE";
+    };
+    
+    TcHmi.Functions.registerFunction('EnumValueConverter', EnumValueConverter);
+})(TcHmi);

009b7faaae8a8adb42f501733103ffb2887b3cf9
kameshwarank-intimetec <kameshwaran.k@intimetec.com>
2026-03-16 16:46:38 +0530

    more implementation

---
+++ Assignment_Week4/Machine_HMI/Functions/AlarmSignalColor.js
@@ -0,0 +1,21 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var AlarmSignalColor = function (eAlarmState) {
+        if (eAlarmState === 0) {
+            return { "color": '#00FF00' };
+        }
+        else
+            return { "color": '#FF0000' }
+    };
+    
+    TcHmi.Functions.registerFunction('AlarmSignalColor', AlarmSignalColor);
+})(TcHmi);

7bbd9cc1f3fbeb7018711d812a6087c246500ee8
kameshwarank-intimetec <kameshwaran.k@intimetec.com>
2026-03-16 21:40:29 +0530

    more implementation on hmi

---
+++ AssignmentWeek3/Hmi_Wash/Functions/EnumModeChange.js
@@ -0,0 +1,27 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var EnumModeChange = function (eMachineState) {
+        let StateArray = [
+            "IDLE",
+            "CONFIGURING",
+            "FILLING",
+            "WASHING",
+            "DRAINING",
+            "COMPLETED",
+            "DRYING",
+            "ERROR"
+        ];
+        return StateArray[eMachineState];
+    };
+    
+    TcHmi.Functions.registerFunction('EnumModeChange', EnumModeChange);
+})(TcHmi);

28f5457ef2a24915f75a42d291c86e624ebf2888
kameshwarank-intimetec <kameshwaran.k@intimetec.com>
2026-04-09 14:14:51 +0530

    hmi and logic refined

---
+++ WarehouseAutomation/WareHouse_HMI/Functions/FunctionJS1.js
@@ -0,0 +1,16 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var FunctionJS1 = function (par1) {
+    };
+    
+    TcHmi.Functions.registerFunction('FunctionJS1', FunctionJS1);
+})(TcHmi);
